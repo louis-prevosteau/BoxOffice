e845a959c7604c4b03679c3077900f79c779c329
louis-prevosteau <louis.prevosteau@laposte.net>
2020-11-02 07:33:24 +0100

    :feat:ajout de la classe Film

---
+++ src/Part1/Film.java
@@ -0,0 +1,34 @@
+package Part1;
+
+public class Film {
+
+    private String titre, réalisateur;
+    private int année, nbEntrées;
+
+    public Film(String titre, String réalisateur, int année, int nbEntrées) {
+        this.titre = titre;
+        this.réalisateur = réalisateur;
+        this.année = année;
+        this.nbEntrées = nbEntrées;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getRéalisateur() {
+        return réalisateur;
+    }
+
+    public int getAnnée() {
+        return année;
+    }
+
+    public int getNbEntrées() {
+        return nbEntrées;
+    }
+
+    public void setNbEntrées(int nbEntrées) {
+        this.nbEntrées = nbEntrées;
+    }
+}
