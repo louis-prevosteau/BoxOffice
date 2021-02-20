6f0f229982c125dd3e7e44ed1c40a2ab565f8c80
louis-prevosteau <louis.prevosteau@laposte.net>
2021-02-20 11:20:41 +0100

    :feat: modification du Main avec possibilité de choisir la structure de données à éxecuter.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
-import boxoffice.BoxOfficeArbre;
-import boxoffice.BoxOfficeChaine;
-import boxoffice.BoxOfficeHash;
-import boxoffice.BoxOfficeTableau;
+import boxoffice.*;
+import java.util.Scanner;
+
+/**
+ * @author Louis PREVOSTEAU
+ */
 
 public class Main {
 
-    /**
-     * @author Louis PREVOSTEAU
-     */
-
     public static void main(String[] args) {
-        System.out.println("Fichier : " + args[0] + "\n");
+        System.out.println("Fichier : " + args[0]);
         System.out.println("----------");
-        BoxOfficeTableau.run(args[0]);
-        BoxOfficeChaine.run(args[0]);
-        BoxOfficeArbre.run(args[0]);
-        BoxOfficeHash.run(args[0]);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Entrez une structure de données : ");
+        String struct = input.next();
+        switch (struct.toLowerCase()){
+            case "tableau" :
+                BoxOfficeTableau.run(args[0]);
+                break;
+            case "liste chaînée" :
+                BoxOfficeChaine.run(args[0]);
+                break;
+            case "abr" :
+                BoxOfficeArbre.run(args[0]);
+                break;
+            case "hashmap" :
+                BoxOfficeHash.run(args[0]);
+                break;
+            case "all" :
+                BoxOfficeTableau.run(args[0]);
+                BoxOfficeChaine.run(args[0]);
+                BoxOfficeArbre.run(args[0]);
+                BoxOfficeHash.run(args[0]);
+                break;
+            default:
+                System.out.println("Cette structure de données n'existe pas.");
+                System.out.println("Choisissez parmi les structures de données suivantes : Tableau, Liste chaînée, ABR, HashMap ou All.");
+                break;
+        }
+        input.close();
     }
 }
